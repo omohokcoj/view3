39a4b67ad58d257ee3a9963cfd6f490ff717d9a2
Pete Matsyburka <pete.matsy@gmail.com>
2021-08-10 12:05:33 +0300

    remove deepmerge

--- src/locale/index.js
+++ src/locale/index.js
@@ -1,20 +1,12 @@
 import defaultLang from './lang/en-US'
-import deepmerge from 'deepmerge'
 import Format from './format'
 
 const format = Format()
 let lang = defaultLang
-let merged = false
 let i18nHandler = function () {
   const vuei18n = Object.getPrototypeOf(this || Vue).$t
+
   if (typeof vuei18n === 'function' && !!Vue.locale) {
-    if (!merged) {
-      merged = true
-      Vue.locale(
-        Vue.config.lang,
-        deepmerge(lang, Vue.locale(Vue.config.lang) || {}, { clone: true })
-      )
-    }
     return vuei18n.apply(this, arguments)
   }
 }
